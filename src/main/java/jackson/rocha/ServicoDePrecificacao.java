0cdb4498666cd753275c4eeab5683fd54c8fb18e
jacksonrocha <jackson_lbr@hotmail.com>
2024-08-17 11:23:14 -0300

    Criando o calculo da embalagem do produto

--- src/main/java/jackson/rocha/ServicoDePrecificacao.java
+++ src/main/java/jackson/rocha/ServicoDePrecificacao.java
@@ -1,6 +1,11 @@
 package jackson.rocha;
 
 public class ServicoDePrecificacao {
-    public void definirPrecoVenda(double v) {
+    public void definirPrecoVenda(Produto produto, double porcentualMargemLucro) {
+
+        double precoVendaCalculado = produto.precoCusto * ((porcentualMargemLucro / 100) + 1);
+        precoVendaCalculado += Produto.custoEmbalagem;
+
+        produto.precoVenda = precoVendaCalculado;
     }
 }
