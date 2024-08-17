d9c2639a8863a004480d1cd581eb52547f839749
jacksonrocha <jackson_lbr@hotmail.com>
2024-08-17 11:22:31 -0300

    Inserindo preço da embalagem

--- src/main/java/jackson/rocha/Principal.java
+++ src/main/java/jackson/rocha/Principal.java
@@ -2,12 +2,13 @@ package jackson.rocha;
 
 public class Principal {
     public static void main(String[] args) {
+        Produto.custoEmbalagem = 10;
 
         Produto novoProduto = new Produto();
         novoProduto.alterarPrecoCusto(80);
 
         ServicoDePrecificacao servicoPrecificacao = new ServicoDePrecificacao();
-        servicoPrecificacao.definirPrecoVenda(novoProduto.precoCusto * 1.5);
+        servicoPrecificacao.definirPrecoVenda(novoProduto, 20);
 
         System.out.printf("Preço de custo: %.2f\n", novoProduto.precoCusto);
         System.out.printf("Preço de venda: %.2f%n", novoProduto.precoVenda);
